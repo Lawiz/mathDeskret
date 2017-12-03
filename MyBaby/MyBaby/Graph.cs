8bca2b84be4a666f8d7f738697b051aad6518a26
peter <petrsobolev1432@gmail.com>
2017-12-04 00:59:45 +0300

    i fuck you

---
+++ MyBaby/MyBaby/Graph.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace MyBaby
+{
+	class Graph
+	{
+
+		public List<GraphNode> listNode{ get; set; }
+		public List<GrapgEdge>listEdge{ get; set; }
+		public void addNode(int count)
+		{
+			for(int countOfNods=0;countOfNods<count;countOfNods++)
+			{
+				Random rnd = new Random((int)DateTime.Now.Ticks);
+				GraphNode node = new GraphNode(rnd.Next(680), rnd.Next(420));
+				listNode.Add(node);
+			}
+		}
+		public void addEdge(GraphNode start, GraphNode finish, bool directed)
+		{
+			listEdge.Add(new GrapgEdge(start, finish, directed));
+		}
+		
+	}
+}
---
+++ MyBaby/MyBaby/GraphNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBaby
+{
+	class GraphNode
+	{
+		public int xPosition{ get; set; }
+		public int yPosition{ get; set; }
+		public GraphNode(int x,int y)
+		{
+			xPosition = x;
+			yPosition = y;
+		}
+		
+	}
+}
